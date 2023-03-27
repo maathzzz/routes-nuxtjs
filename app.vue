f70da7e8265f730a2af775afe3dc35f59c9c37cc
Matheus <matheusdeamorim20@gmail.com>
2023-03-27 15:50:00 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1> Baby Name Generator </h1>
+    <p> Choose your options and click the "Find Names" button below.</p>
+    <div class="options-container">
+      <div class="option-container">
+          <h4> 1 - Choose a gender </h4>
+          <div class="option-buttons">
+            <button> Boy </button>
+            <button> Girl </button>
+          </div>
+          <div class="option-container"></div>
+          <div class="option-container"></div>
+      </div>
+    </div>
+  </div>
+</template>
